82051f6af53bfe3e92c1ecbac76511d2f150a6cc
Andrii Androsovych <55437123+aaw20904@users.noreply.github.com>
2026-03-24 10:22:23 +0200

    Update base_snippets_async.c

--- snippets/base_snippets_async.c
+++ snippets/base_snippets_async.c
@@ -1,11 +1,96 @@
 
- /******Find USB device by PID, VID, BCD***/
+
+///a calback for debug mode
+
+ void  dbgMessageCallback (libusb_context *ctx, enum libusb_log_level level, const char *str) {
+     //NOTE When the console is not available in app - use OWN ,another callback realization to avoid app crash
+   printf("%s , \n", str);
+ }
+
+/**********open , init the library*********/
+int initLibraryX1( uint16_t fifoSize, uint16_t  endpointsRamSize, bool debug) {
+  myUsbDeviceHead.myDevicehandle = NULL; //util will opened any device
+    //1)Upload  executable code from the .dll inside app
+ if (load_libusb() != 0) {
+    return -1;
+ }
+   //2)Allocate memory for the FIFO (IN endpoint stream reconstructing)
+  userEndpointsArrays.fifosArray = malloc(fifoSize);
+  userEndpointsArrays.inEpArray = malloc(endpointsRamSize);
+  userEndpointsArrays.outEpArray = malloc(endpointsRamSize);
+  if ( (userEndpointsArrays.fifosArray == NULL) || (userEndpointsArrays.inEpArray == NULL) || (userEndpointsArrays.outEpArray== NULL) ) {
+   return -1;
+  }
+  //3)initializing the libusb context
+  if (libusb_init_d(&myLibUsbContext) != 0) {
+    myLibUsbContext = NULL;
+    return -1;
+  }
+
+   //4)add debug features
+     if (debug) {
+            myUsbDeviceHead.isDebug = true;
+            //turn debug info , set debug message level
+           libusb_set_debug_d(myLibUsbContext, LIBUSB_LOG_LEVEL_WARNING);
+           //attach the callback to the given context
+           libusb_set_log_cb_d(myLibUsbContext, dbgMessageCallback, LIBUSB_LOG_CB_CONTEXT);
+
+     }
+
+  //end) When OK
+  return 0;
+}
+ 
+/********************************************************/
+int deInitLibraryX1 ( void ) {
+    //close a device
+    if (myUsbDeviceHead.myDevicehandle != NULL) {
+        //release interface
+        libusb_release_interface_d (myUsbDeviceHead.myDevicehandle,0);
+        //close device
+        libusb_close_d (myUsbDeviceHead.myDevicehandle);
+        myUsbDeviceHead.myDevicehandle = NULL;
+    }
+       //1)free memory, if exist
+     if (userEndpointsArrays.fifosArray != NULL) {
+        free(userEndpointsArrays.fifosArray);
+     }
+
+     if (userEndpointsArrays.inEpArray != NULL) {
+        free(userEndpointsArrays.inEpArray);
+     }
+
+     if (userEndpointsArrays.outEpArray != NULL) {
+        free(userEndpointsArrays.outEpArray);
+     }
+      //close cotext of libusb
+      if (myLibUsbContext != NULL) {
+        libusb_exit_d(myLibUsbContext);
+      }
+
+       //2)release RAM from executable code of DLL, uploaded lated
+      unload_libusb();
+      return 0;
+}
+/*******************************************/
+//Search params of the usb device .Must be initialized!
+void assignDeviceParamsForSearchX1(uint16_t pid, uint16_t vid, uint16_t bcd ){
+  myUsbDeviceHead.pid = pid;
+  myUsbDeviceHead.vid = vid;
+  myUsbDeviceHead.bcd = bcd;
+}
+
+
+/******Find USB device by PID, VID, BCD***/
 /*
 NOTE - there may be the  next warnings after closing the lirary  , when the  debug mode turns on in the libusb:
 libusb: warning [libusb_exit] device 1.0 still referenced , libusb: warning [libusb_exit] device 1.5 still referenced
  People in the Internet talk, that it is a bug of library.For example, in MacOS an Linux it is one cases ,but absent in other.
 */
-int findUsbDeviceX1 (void) {
+//This function requires a list of connected devices from a system,
+// when a device was disconnected and appeared again - connect, get descriptor, clam and connect it again
+// when a device was connected and disappeared again - close the device, claims the interface
+   int findUsbDeviceX1 (void) {
        libusb_device* myDevice = NULL;
        libusb_device**  listOfConnected;
        ssize_t     howManyWasFound;
@@ -13,10 +98,6 @@ int findUsbDeviceX1 (void) {
 
        //1) get the list of connected devices
        howManyWasFound = libusb_get_device_list_d(myLibUsbContext, &listOfConnected);
-      //when there was any error during  requestion of the list:
-      if (howManyWasFound < 0) {
-        return -1;
-      }
 
        if (howManyWasFound == 0) {
             //no connected devices was found
@@ -29,44 +110,44 @@ int findUsbDeviceX1 (void) {
        for (int devI=0; devI<howManyWasFound; devI++) {
              //a)get descriptor
              libusb_get_device_descriptor_d( listOfConnected[devI],&myDevDescriptor);
-             //b)Compare PID, VID, BCD
-             if ((myDevDescriptor.idProduct == myUsbDeviceHead.pid) && (myDevDescriptor.idVendor == myUsbDeviceHead.vid) && (myDevDescriptor.idVendor == myUsbDeviceHead.bcd) ){
+             //b)Compare PID, VID
+             if ((myDevDescriptor.bcdDevice == myUsbDeviceHead.bcd) && (myDevDescriptor.idProduct == myUsbDeviceHead.pid) && (myDevDescriptor.idVendor == myUsbDeviceHead.vid)){
                 //when matched - assign a device
                 myDevice = listOfConnected[devI];
-               break;
+                break;
              }
        }
-       ////if the device with exactly PID, VID, VID was found:
+       //if the device with exactly PID, VID was found:
        if (myDevice != NULL) {
             //3)Connection with libusb_open
-            
              if (libusb_open_d(myDevice, &myUsbDeviceHead.myDevicehandle) != 0){
-                   //when open was fail
+                    //when open was fail
                 libusb_free_device_list_d(listOfConnected,1);
-                  //clear handle
-                myUsbDeviceHead.myDevicehandle = NULL;
+                //clear handle
+               myUsbDeviceHead.myDevicehandle = NULL;
                 return -1;
              }
-          
             //4)Claim the interface with number 0
              if (libusb_claim_interface_d(myUsbDeviceHead.myDevicehandle,0) !=0 ){
-              //close device
+                //close device
                 libusb_close_d (myUsbDeviceHead.myDevicehandle);
-                //free the list
                 libusb_free_device_list_d(listOfConnected,1);
-                    //clear handle
-               myUsbDeviceHead.myDevicehandle = NULL;
                return -1;
              }
+              //5)Clean the list
+            libusb_free_device_list_d(listOfConnected,1);
+            return 0;
+
+       } else {
+          //clear handle, because device ws not found
+            myUsbDeviceHead.myDevicehandle = NULL;
+            //5)Clean the  list
+            libusb_free_device_list_d(listOfConnected,1);
             return 1;
        }
 
-       //4)Clean the found list
-       libusb_free_device_list_d(listOfConnected,1);
-       return 0;
 
    }
-
 /*************************************************************************
 this progam search a device with exactly PID,VID,bcdDevice. 
 When device not found - release the interface 0, close the device, handle=NULL
